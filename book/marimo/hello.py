2042017c043b2836d913ce6db665b81b512aa482
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-08-07 08:06:08 +0200

    Hello World with Marimo

---
+++ book/marimo/hello.py
@@ -0,0 +1,25 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "marimo==0.13.15"
+# ]
+# ///
+"""Marimo Hello World."""
+
+import marimo
+
+__generated_with = "0.13.15"
+app = marimo.App()
+
+with app.setup:
+    import marimo as mo
+
+
+@app.cell
+def _():
+    mo.md(r"""# Marimo Hello World""")
+    return
+
+
+if __name__ == "__main__":
+    app.run()
