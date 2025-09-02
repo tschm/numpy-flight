40214c312d0f3438a46c3d437393114ce8893e93
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-09-02 10:20:26 +0200

    Delete src/tests directory (#135)

--- src/tests/test_utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Test package for the utility modules of the NumPy over Apache Arrow Flight package.
-
-This package contains test modules for verifying the functionality of the utility modules
-in the flight.utils package.
-"""
